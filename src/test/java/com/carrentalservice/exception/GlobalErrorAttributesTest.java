77f114130b13c4692534c93cebc65d68340425f6
George Niculae <george.niculae20@yahoo.com>
2023-06-12 00:29:57 +0300

    Update

---
+++ src/test/java/com/carrentalservice/exception/GlobalErrorAttributesTest.java
@@ -0,0 +1,36 @@
+package com.carrentalservice.exception;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class GlobalErrorAttributesTest {
+
+    private final GlobalErrorAttributes globalErrorAttributes = new GlobalErrorAttributes();
+
+    @Test
+    void handleNotFoundExceptionTest() {
+        NotFoundException notFoundException = new NotFoundException("Resource not found");
+
+        MockHttpServletRequest servletRequest = new MockHttpServletRequest();
+        servletRequest.setServerName("car-rental-service");
+        servletRequest.setRequestURI("/employee");
+        servletRequest.addParameter("name", "value");
+        WebRequest webRequest = new ServletWebRequest(servletRequest);
+
+        ResponseEntity<Map<String, Object>> responseEntity =
+                globalErrorAttributes.handleNotFoundException(notFoundException, webRequest);
+
+        assertEquals(404, responseEntity.getStatusCode().value());
+    }
+
+}
--- src/test/java/com/carrentalservice/service/CarServiceTest.java
+++ src/test/java/com/carrentalservice/service/CarServiceTest.java
@@ -1,10 +1,21 @@
 package com.carrentalservice.service;
 
+import com.carrentalservice.entity.Car;
+import com.carrentalservice.exception.NotFoundException;
 import com.carrentalservice.repository.CarRepository;
+import com.carrentalservice.util.TestData;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CarServiceTest {
@@ -14,5 +25,39 @@ class CarServiceTest {
 
     @Mock
     private CarRepository carRepository;
+
+    @Test
+    void findCarByIdTest_success() {
+        Car car = TestData.createCar();
+
+        when(carRepository.findById(anyLong())).thenReturn(Optional.of(car));
+
+        assertDoesNotThrow(() -> carService.findCarById(1L));
+        Car actualCar = carService.findCarById(1L);
+
+        assertNotNull(actualCar);
+    }
+
+    @Test
+    void findCarByIdTest_errorOnFindingById() {
+        when(carRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        NotFoundException notFoundException = assertThrows(NotFoundException.class, () -> carService.findCarById(1L));
+
+        assertNotNull(notFoundException);
+    }
+
+    @Test
+    void updateCarTest_success() {
+        Car car = TestData.createCar();
+
+        when(carRepository.findById(anyLong())).thenReturn(Optional.of(car));
+        when(carRepository.save(any(Car.class))).thenReturn(car);
+
+        assertDoesNotThrow(() -> carService.updateCar(car));
+        Car updatedCar = carService.updateCar(car);
+
+        assertNotNull(updatedCar);
+    }
 
 }
