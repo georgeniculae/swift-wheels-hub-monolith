27ff2f3409d84f4a2f08c469745ede1e00086ec0
George Niculae <george.niculae20@yahoo.com>
2023-06-11 14:29:28 +0300

    Update

--- src/main/java/com/carrentalservice/exception/GlobalErrorAttributes.java
+++ src/main/java/com/carrentalservice/exception/GlobalErrorAttributes.java
@@ -2,19 +2,43 @@ package com.carrentalservice.exception;
 
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Map;
 
-@Component
-@Order(-2)
+@RestControllerAdvice
 public class GlobalErrorAttributes extends DefaultErrorAttributes {
 
-    @Override
-    public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
-        return super.getErrorAttributes(webRequest, options);
+    private static final String MESSAGE = "message";
+    private static final String STATUS = "status";
+    private static final String ERROR = "error";
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleNotFoundException(NotFoundException e, WebRequest request) {
+        HttpStatus notFound = HttpStatus.NOT_FOUND;
+        String cause = "Resource not found";
+        Map<String, Object> errorAttributes = getErrorAttributesMap(request, e.getMessage(), cause, notFound);
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorAttributes);
+    }
+
+
+    private Map<String, Object> getErrorAttributesMap(WebRequest webRequest, String errorMessage, String cause, HttpStatus httpStatus) {
+        Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, ErrorAttributeOptions.defaults());
+        addErrorAttributes(errorAttributes, errorMessage, cause, httpStatus);
+
+        return errorAttributes;
+    }
+
+    private void addErrorAttributes(Map<String, Object> errorAttributes, String errorMessage, String cause, HttpStatus httpStatus) {
+        errorAttributes.put(MESSAGE, errorMessage);
+        errorAttributes.put(STATUS, httpStatus.value());
+        errorAttributes.put(ERROR, cause);
     }
 
 }
