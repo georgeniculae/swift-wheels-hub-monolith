218fdd94e917aec791029c386fccc368c9b33dee
George Niculae <george.niculae20@yahoo.com>
2023-06-21 14:23:36 +0300

    Update

--- src/main/java/com/carrentalservice/security/SecurityConfig.java
+++ src/main/java/com/carrentalservice/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+          http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requestMatcherRegistry) -> requestMatcherRegistry
                         .requestMatchers("/register", "/user/register", "/", "/css/**", "/images/**").permitAll()
                         .requestMatchers("/branch/**", "/car/**", "/customer/**", "/employee/**", "/rental-office/**").hasRole("ADMIN")
