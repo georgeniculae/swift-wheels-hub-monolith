e2fda93b189105573e2d4d3f28c7d46152040f39
George Niculae <george.niculae20@yahoo.com>
2023-06-21 11:20:36 +0300

    Update

--- src/main/java/com/carrentalservice/restcontroller/InvoiceRestController.java
+++ src/main/java/com/carrentalservice/restcontroller/InvoiceRestController.java
@@ -1,15 +1,26 @@
 package com.carrentalservice.restcontroller;
 
+import com.carrentalservice.dto.InvoiceDto;
 import com.carrentalservice.service.InvoiceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(path = "/invoice")
 public class InvoiceRestController {
 
     private final InvoiceService invoiceService;
 
+    @PutMapping(path = "/update")
+    public ResponseEntity<InvoiceDto> updateInvoice(@RequestBody InvoiceDto invoiceDto) {
+        InvoiceDto undatedinvoiceDto = invoiceService.updateInvoice(invoiceDto);
 
+        return ResponseEntity.ok(undatedinvoiceDto);
+    }
 
 }
