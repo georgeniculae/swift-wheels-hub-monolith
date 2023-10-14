247994566ad9c30b1d66c6e94c55c4b1889570b3
George Niculae <george.niculae20@yahoo.com>
2023-10-15 01:03:51 +0300

    Update

--- src/main/java/com/carrentalservice/security/UserDetailsConfig.java
+++ src/main/java/com/carrentalservice/security/UserDetailsConfig.java
@@ -1,19 +1,14 @@
 package com.carrentalservice.security;
 
-import com.carrentalservice.entity.User;
 import com.carrentalservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.util.List;
-import java.util.Optional;
 
 @Configuration
 @RequiredArgsConstructor
@@ -23,21 +18,8 @@ public class UserDetailsConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return username -> {
-            Optional<User> optionalUser = userRepository.findByUsername(username);
-
-            if (optionalUser.isPresent()) {
-                User user = optionalUser.get();
-
-                return new org.springframework.security.core.userdetails.User(
-                        user.getUsername(),
-                        user.getPassword(),
-                        List.of(new SimpleGrantedAuthority(user.getRole().getName()))
-                );
-            }
-
-            throw new UsernameNotFoundException("Username not found");
-        };
+        return username -> userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
     }
 
     @Bean
